60e03e7d195832469227a9d0e15ef5c40d77390d
Kevin Clement <kevin.clement@gmail.com>
2020-10-20 18:38:55 -0700

    initial switch to prep for mapjs

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ let logger = new (require('./logging'))
 let run = new (require('./run'))({ logger: logger, db:fb.db })
 
 let managers = [];
-managers.push(new (require('./manager.cabinet'))({ name: 'cabinet', logger: logger, fb: fb, run: run }))
+// TODO: put back before going live again
+// #######################################
+
+// managers.push(new (require('./manager.cabinet'))({ name: 'cabinet', logger: logger, fb: fb, run: run }))
 
 // might want to turn this off while doing dev, so I have a flag for it
 let ENABLE_FIREBASE_LOGS = true;
@@ -11,41 +14,44 @@ if (ENABLE_FIREBASE_LOGS) {
     logger.enableFirebase(fb.db);
 }
 
-logger.log('comp: Started ExitPuzzles Map server.');
+logger.log('mapjs: Started ExitPuzzles Map server.');
 
 // track firebase connect and disconnects and log them so we can see how often it happens
 let _connecting = true;
 fb.db.ref('.info/connected').on('value', function(connectedSnap) {
   if (connectedSnap.val() === true) {
-    logger.log('comp: firebase connected.');
+    logger.log('mapjs: firebase connected.');
   } else {
     // dont print an error while its still connecting on first start
     if (_connecting) {
       _connecting = false;
     } else {
-      logger.log('comp: firebase dropped connection!');
+      logger.log('mapjs: firebase dropped connection!');
     }
   }
 });
 
-// listen for control operations in the db, filter only ops not completed
-fb.db.ref('museum/operations').orderByChild('completed').equalTo(null).on("child_added", function(snapshot) {
-    logger.log('map: received op ' + snapshot.val().command);
+// TODO: ADD BACK
+// #######################################
 
-    managers.forEach((m) => {
-        m.handle(snapshot);
-    });
- });
+// listen for control operations in the db, filter only ops not completed
+// fb.db.ref('museum/operations').orderByChild('completed').equalTo(null).on("child_added", function(snapshot) {
+//     logger.log('map: received op ' + snapshot.val().command);
+
+//     managers.forEach((m) => {
+//         m.handle(snapshot);
+//     });
+//  });
 
 // update started time and set a ping timer
-fb.db.ref('museum/status/pedestal').update({
+fb.db.ref('museum/status/mapjs').update({
     started: (new Date()).toLocaleString(),
     ping: (new Date()).toLocaleString()
 })
 
 // heartbeat timer
 setInterval(()  => {
-    fb.db.ref('museum/status/pedestal').update({
+    fb.db.ref('museum/status/mapjs').update({
       ping: (new Date()).toLocaleString()
     })
 }, 30000)
